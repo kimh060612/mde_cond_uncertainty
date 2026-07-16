37f94dc21a2996d5a78d13e751efae0a421bfb70
kimh060612 <kimh060612@naver.com>
2026-07-17 03:05:59 +0900

    .

--- utils/trainer_camind.py
+++ utils/trainer_camind.py
@@ -212,7 +212,7 @@ def train_one_epoch(
                 out["variance"],
                 target_loss,
             )
-            q_score = out["camera_bias"] # + uncertainty_alpha * out["std"]
+            q_score = out["camera_bias"] + uncertainty_alpha * out["std"]
             ranking_loss = signed_pairwise_ranknet_loss(
                 reshape_group_batch(q_score, num_groups, num_candidates),
                 reshape_group_batch(target_loss, num_groups, num_candidates), # abs_rel_degradation
--- utils/validator_camind.py
+++ utils/validator_camind.py
@@ -226,7 +226,7 @@ def validate(
                 out["variance"],
                 target_loss,
             )
-            q_score = out["camera_bias"] # + uncertainty_alpha * out["std"]
+            q_score = out["camera_bias"] + uncertainty_alpha * out["std"]
             group_q = reshape_group_batch(q_score, num_groups, num_candidates)
             group_degradation = reshape_group_batch(
                 target_loss,  # abs_rel_degradation,
