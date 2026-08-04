0772d8968d96cd27f72aa3ff2c6eb92989dec5f4
kimh060612 <kimh060612@naver.com>
2026-08-05 01:16:47 +0900

    loss

--- model/loss_target.py
+++ model/loss_target.py
@@ -429,8 +429,8 @@ def ssi_depth_guided_meter_space_depth_loss(
         eps=eps,
     )["depth"]
 
-    candidate_depth_diff = torch.abs(candidate_gt_depth - canonical_meter_depth) / (candidate_gt_depth.clamp_min(eps))
-    canonical_depth_diff = torch.abs(canonical_gt_depth - candidate_meter_depth) / (canonical_gt_depth.clamp_min(eps))
+    candidate_depth_diff = torch.abs(candidate_gt_depth - candidate_meter_depth) / (candidate_gt_depth.clamp_min(eps))
+    canonical_depth_diff = torch.abs(canonical_gt_depth - canonical_meter_depth) / (canonical_gt_depth.clamp_min(eps))
     
     comparison_valid = (
         candidate_valid
